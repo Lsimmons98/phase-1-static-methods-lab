38b3cd64d674be7f664964ec02654245076a7911
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-05 15:32:32 -0400

    Create Formatter class with capitalize, sanitize, and titeize methods

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    const firstLetter = string.charAt(0).toUpperCase()
+    const stringMinusFirst = string.slice(1)
+    return firstLetter + stringMinusFirst
+  }
+
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9-'\s]/g, '')
+  }
+
+  static titleize(string){
+    const smallWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = string.toLowerCase().split(' ');
+
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !smallWords.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    })
+    return titleizedWords.join(' ')
+  }
 }
+
+
+
+Formatter.capitalize('yoyo')
